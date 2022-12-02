1dc5f254986bd986fbe4ac4c14e24f5b5b8b8143
Andre Gavino <andre.gavino@g.globo>
2022-12-02 11:21:11 -0300

    test: Await javascript load before run

--- testingLibrary.test.js
+++ testingLibrary.test.js
@@ -1,33 +1,43 @@
-/**
- * @jest-environment jsdom
- */
-
-// query utilities:
-// var assert = require('node:assert');
-const testingLibraryDom = require("@testing-library/dom");
-const { getByLabelText, getByText, getByTestId, queryByTestId, waitFor, screen, fireEvent } = testingLibraryDom;
 require("@testing-library/jest-dom");
-const { readFileSync } = require('node:fs');
+const { logDOM, getByText, getByTestId, waitFor, fireEvent } = require("@testing-library/dom");
+const { JSDOM } = require("jsdom");
+const fs = require("fs");
+const path = require("path");
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
 
 
-  function getExampleDOM() {
-    var data = readFileSync('index.html', 'utf8');
-    //console.log(data)
-                      
-    // This is just a raw example of setting up some DOM
-    // that we can interact with. Swap this with your UI
-    // framework of choice 😉
-    const div = document.createElement('div');
-    div.innerHTML = data;
-    return div
-  }
-  
-  test('examples of some things', async () => {
-    const container = getExampleDOM()
-    var teclaUm = getByText(container, '1')
-    var display = getByTestId(container, 'display')
-    fireEvent.click(teclaUm);
-    await waitFor(() =>
-      expect(display).toHaveTextContent('1')
-    )
-  })
+const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+let dom;
+let container;
+
+function waitForDom() {
+  return new Promise((resolve) => {
+    dom = new JSDOM(html, {
+      runScripts: "dangerously",
+      resources: "usable",
+      url: `file://${path.resolve(__dirname, ".")}/index.html`,
+    });
+    dom.window.document.addEventListener("DOMContentLoaded", () => {
+      resolve();
+    });
+  });
+}
+
+beforeAll(async () => await waitForDom());
+
+beforeEach(() => {
+  container = dom.window.document.body;
+});
+
+afterEach(() => container = null)
+
+
+it("examples of some things", async () => {
+  var teclaUm = getByText(container, '1')
+  var display = getByTestId(container, 'display')
+  fireEvent.click(teclaUm);
+  await waitFor(() =>
+    expect(display).toHaveTextContent('1'), {container}
+  )
+});
